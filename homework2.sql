fe0659c4fb6466b68aa27f8c6c3c682e6a7b2e12
Alexey <53167555+Alex-dicson@users.noreply.github.com>
2024-03-28 18:30:46 +0300

    Update homework2.sql

--- homework2.sql
+++ homework2.sql
@@ -5,25 +5,40 @@ CREATE TABLE IF NOT EXISTS genres(
 
 CREATE TABLE IF NOT EXISTS performers(
     id integer PRIMARY KEY,
-    name varchar(80) NOT NULL,
-    ganre integer NOT NULL REFERENCES genres(id)
+    name varchar(80) NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS albums(
     id integer PRIMARY KEY,
     name varchar(80) NOT NULL,
-    year_release integer NOT NULL,
-    performer integer NOT NULL REFERENCES performers(id)
-);  
+    year_release integer NOT NULL
+); 
+
 CREATE TABLE IF NOT EXISTS tracks(
     id integer PRIMARY KEY,
     name varchar(80) UNIQUE NOT NULL,
-    duration time NOT NULL,
-    album integer NOT NULL REFERENCES albums(id)
+    duration time NOT NULL
 );  
+
 CREATE TABLE IF NOT EXISTS collections(
     id integer PRIMARY KEY,
     name varchar(80) NOT NULL,
     year_release integer NOT NULL, 
     track integer NOT NULL REFERENCES tracks(id),
     album integer NOT NULL REFERENCES albums(id)
+);
+
+CREATE TABLE IF NOT EXISTS PerformerGenre(
+    genre integer NOT NULL REFERENCES genres(id),
+    album integer NOT NULL REFERENCES albums(id)
+);
+
+CREATE TABLE IF NOT EXISTS AlbumsPerformers(
+    album integer NOT NULL REFERENCES albums(id),
+    performer integer NOT NULL REFERENCES performers(id)
+);
+
+CREATE TABLE IF NOT EXISTS ColletionsTracks(
+    collection integer NOT NULL REFERENCES collections(id),
+    track integer NOT NULL REFERENCES tracks(id)
 );
