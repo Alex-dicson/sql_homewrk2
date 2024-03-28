907b6019951f39fb7401718b904d8825c04de190
Alexey <53167555+Alex-dicson@users.noreply.github.com>
2024-03-28 18:43:27 +0300

    Update homework2.sql

--- homework2.sql
+++ homework2.sql
@@ -8,29 +8,28 @@ CREATE TABLE IF NOT EXISTS performers(
     name varchar(80) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS albums(
-    id integer PRIMARY KEY,
-    name varchar(80) NOT NULL,
-    year_release integer NOT NULL
-); 
-
 CREATE TABLE IF NOT EXISTS tracks(
     id integer PRIMARY KEY,
     name varchar(80) UNIQUE NOT NULL,
     duration time NOT NULL
 );  
 
+CREATE TABLE IF NOT EXISTS albums(
+    id integer PRIMARY KEY,
+    name varchar(80) NOT NULL,
+    year_release integer NOT NULL,
+    track integer NOT NULL REFERENCES tracks(id)
+); 
+
 CREATE TABLE IF NOT EXISTS collections(
     id integer PRIMARY KEY,
     name varchar(80) NOT NULL,
-    year_release integer NOT NULL, 
-    track integer NOT NULL REFERENCES tracks(id),
-    album integer NOT NULL REFERENCES albums(id)
+    year_release integer NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS PerformerGenre(
     genre integer NOT NULL REFERENCES genres(id),
-    album integer NOT NULL REFERENCES albums(id)
+    performer integer NOT NULL REFERENCES performers(id)
 );
 
 CREATE TABLE IF NOT EXISTS AlbumsPerformers(
